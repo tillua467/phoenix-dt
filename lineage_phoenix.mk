fbcd17bce4f1930f874a5533009d281ccbd7df17
tillua467 <tillua467@gmail.com>
2024-12-16 13:34:27 +0600

    Bring up Evo stuff and added some extras

--- lineage_phoenix.mk
+++ lineage_phoenix.mk
@@ -11,16 +11,35 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from phoenix device
 $(call inherit-product, device/xiaomi/phoenix/device.mk)
 
-# Inherit some common Lineage stuff.
+# Inherit some common EVO stuff.
+TARGET_DISABLE_EPPE := true
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+EVO_BUILD_TYPE := Unofficial
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+TARGET_PHONE_LINK_SUPPORTED := true
+TARGET_USES_PICO_GAPPS := true
 
+# Custom stuff.
+TARGET_BUILD_APERTURE_CAMERA := true
+TARGET_USE_PIXEL_FINGERPRINT := true
+TARGET_INCLUDE_PIXEL_CHARGER := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_ENABLE_BLUR := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_phoenix
 PRODUCT_DEVICE := phoenix
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi K30
 PRODUCT_MANUFACTURER := Xiaomi
+TARGET_VENDOR := Xiaomi
+BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+TARGET_VENDOR_PRODUCT_NAME := phoenix
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="phoenix-user 11 RKQ1.200826.002 21.9.28 release-keys" \
