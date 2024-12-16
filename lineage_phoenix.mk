1f2d225e1540e57074f5bf8f8ec648ad2fb4ffff
root <root@Tanisha.localdomain>
2024-12-16 13:24:45 +0600

    Revert "phoenix: bringup aosPB"
    This reverts commit 413a039761dbfc43064a01e520e9988dcb0cd463.

--- lineage_phoenix.mk
+++ lineage_phoenix.mk
@@ -11,14 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from phoenix device
 $(call inherit-product, device/xiaomi/phoenix/device.mk)
 
-# Inherit some common aosPB stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Inherit some flags for aosPB
-TARGET_FACE_UNLOCK_SUPPORTED := true
-CUSTOM_BUILDTYPE := OFFICIAL
-
-PRODUCT_NAME := aosp_phoenix
+PRODUCT_NAME := lineage_phoenix
 PRODUCT_DEVICE := phoenix
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi K30
