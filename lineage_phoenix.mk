e52304cf423ceefff494f012660215b2b1d1c994
Tillua467 <128046674+tillua467@users.noreply.github.com>
2024-12-20 15:38:06 +0600

    Update lineage_phoenix.mk: Mist Additional bring up

--- lineage_phoenix.mk
+++ lineage_phoenix.mk
@@ -22,13 +22,15 @@ TARGET_DEFAULT_PIXEL_LAUNCHER := true
 
 # Custom stuff.
 PRODUCT_NO_CAMERA := false
+TARGET_EXCLUDES_AUDIOFX := true
 TARGET_USE_PIXEL_FINGERPRINT := true
 TARGET_INCLUDE_PIXEL_CHARGER := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_ENABLE_BLUR := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_FINGERPRINT_UNLOCK_SUPPORTED := true
-TARGET_USE_GOOGLE_TELEPHONY := true
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := false
+MIST_BUILD_TYPE := UNOFFICIAL
 
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_phoenix
@@ -38,16 +40,16 @@ PRODUCT_MODEL := Redmi K30
 PRODUCT_MANUFACTURER := Xiaomi
 TARGET_VENDOR := Xiaomi
 BOARD_VENDOR := Xiaomi
+PROCESSOR_MODEL := SM6150
 
 MISTOS_MAINTAINER="Tillua467"
 
-# SOC
-PROCESSOR_MODEL := SM6150
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 TARGET_VENDOR_PRODUCT_NAME := phoenix
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="phoenix-user 11 RKQ1.200826.002 21.9.28 release-keys" \
-    BuildFingerprint=Redmi/phoenix/phoenix:11/RKQ1.200826.002/21.9.28:user/release-keys
+    BuildDesc="phoenix-user 11 RKQ1.200826.002 21.9.28 release-keys" 
+
+BuildFingerprint := Redmi/phoenix/phoenix:11/RKQ1.200826.002/21.9.28:user/release-keys
