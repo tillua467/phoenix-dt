dcc1b9aa2168803763659067f082a13dfff43f34
tillua467 <tillua467@gmail.com>
2025-02-05 13:37:35 +0600

    phoenix: BringUp!: int @Evolution-X

--- lineage_phoenix.mk
+++ lineage_phoenix.mk
@@ -14,13 +14,35 @@ $(call inherit-product, device/xiaomi/phoenix/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+WITH_GAPPS := true
+TARGET_BUILD_GOOGLE_TELEPHONY := true
+TARGET_USES_MINI_GAPPS := true
+TARGET_DISABLE_EPPE := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_BUILD_APERTURE_CAMERA := false
+TARGET_USE_PIXEL_FINGERPRINT := true
+TARGET_INCLUDE_PIXEL_CHARGER := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_ENABLE_BLUR := true
+TARGET_SUPPORT_BOOT_ANIMATIONS := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_FINGERPRINT_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_ACCORD := false
+
+# Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_phoenix
 PRODUCT_DEVICE := phoenix
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi K30
 PRODUCT_MANUFACTURER := Xiaomi
-
+TARGET_VENDOR := Xiaomi
+BOARD_VENDOR := Xiaomi
+TARGET_VENDOR_PRODUCT_NAME := phoenix
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="phoenix-user 11 RKQ1.200826.002 21.9.28 release-keys" \
