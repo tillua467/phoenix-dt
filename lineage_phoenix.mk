069e0381c159eb4982fdd296ae6cda8fa581f257
tillua467 <tillua467@gmail.com>
2025-02-06 22:42:02 +0600

    phoenix:BringUp: Let's Build Matrixx!

--- lineage_phoenix.mk
+++ lineage_phoenix.mk
@@ -11,14 +11,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from phoenix device
 $(call inherit-product, device/xiaomi/phoenix/device.mk)
 
-# Inherit some common Lineage stuff.
+# Inherit some common Matrixx stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
-WITH_GAPPS := true
+WITH_GMS := true
 TARGET_BUILD_GOOGLE_TELEPHONY := true
-TARGET_USES_MINI_GAPPS := true
 TARGET_DISABLE_EPPE := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
@@ -27,10 +26,13 @@ TARGET_USE_PIXEL_FINGERPRINT := true
 TARGET_INCLUDE_PIXEL_CHARGER := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_ENABLE_BLUR := true
-TARGET_SUPPORT_BOOT_ANIMATIONS := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_FINGERPRINT_UNLOCK_SUPPORTED := true
 TARGET_INCLUDE_ACCORD := false
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_STOCK_AICORE := true
+TARGET_INCLUDE_NEXUS := false
 
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_phoenix
